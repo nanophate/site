436e8bfefe2a9198dfa055b59fa08630f55880e5
nanophate <nanophate@me.com>
2021-07-15 18:06:23 +0900

    Change clickk after text

--- spec/feature/app_spec.rb
+++ spec/feature/app_spec.rb
@@ -4,9 +4,9 @@ describe 'app', type: :feature, js: true do
   it 'clicks greeting to change welcome message' do
     visit '/'
     expect(page).to have_content('Hello, Thank you for accessing to this website.')
-    expect(page).not_to have_content('Hi, Thank you for clicking!')
+    expect(page).not_to have_content('Hi, Thank you for touch me!')
     click_on 'Greeting!'
     expect(page).not_to have_content('Hello, Thank you for accessing to this website.')
-    expect(page).to have_content('Hi, Thank you for clicking!')
+    expect(page).to have_content('Hi, Thank you for touch me!')
   end
 end
